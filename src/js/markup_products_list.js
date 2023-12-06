f54fe24b21e2eab03b7b74729e6ca6796bfe7c60
inhasid <sidorchuk.inga@gmail.com>
2023-12-06 23:50:50 +0100

    markup-product-list

---
+++ src/js/markup_products_list.js
@@ -0,0 +1,32 @@
+export default function createMarkup(arr) {
+    return arr
+        .map(
+            ({
+                name,
+                img,
+                category,
+                price,
+                size,
+                popularit,
+            }) => `
+            <li class="product-item">
+              <div class="product-img-container">
+                <img src="${img}" alt="${name}" width="140" height="140" class="product-img">
+              </div>
+              <h3 class="product-title">${name}</h3>
+              <p class="product-parameter">Category: <span class="span-parameter-value">${category}</span></p>
+              <p class="product-parameter">Size: <span class="span-parameter-value">${size}</span></p>
+              <p class="product-parameter">Popularity: <span class="span-parameter-value">${popularit}</span></p>
+              <div class="price-card-container">
+                <p class="price">${price}</p>
+                <button class="cart-button" type="submit">
+                  <svg class="cart-icon" width="18" height="18">
+                    <use href="./img/symbol-defs.svg#icon-basket"></use>
+                  </svg>
+                </button>
+              </div>
+            </li>
+            `
+    )
+    .join("");
+}
