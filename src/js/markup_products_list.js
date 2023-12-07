a034a9e16ee674e30e8b14a53b4b7ac835428364
inhasid <sidorchuk.inga@gmail.com>
2023-12-07 09:49:40 +0100

    product list css

--- src/js/markup_products_list.js
+++ src/js/markup_products_list.js
@@ -14,9 +14,11 @@ export default function createMarkup(arr) {
                 <img src="${img}" alt="${name}" width="140" height="140" class="product-img">
               </div>
               <h3 class="product-title">${name}</h3>
-              <p class="product-parameter">Category: <span class="span-parameter-value">${category}</span></p>
-              <p class="product-parameter">Size: <span class="span-parameter-value">${size}</span></p>
-              <p class="product-parameter">Popularity: <span class="span-parameter-value">${popularit}</span></p>
+              <div class="product-parameter-container">
+                <p class="product-parameter">Category: <span class="span-parameter-value">${category}</span></p>
+                <p class="product-parameter">Size: <span class="span-parameter-value">${size}</span></p>
+                <p class="product-parameter">Popularity: <span class="span-parameter-value">${popularit}</span></p>
+                </div>
               <div class="price-card-container">
                 <p class="price">${price}</p>
                 <button class="cart-button" type="submit">
