d27b058ce5b554cd57798c5dad611d1a2758b166
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-12-09 10:03:38 +0000

    Deploying to gh-pages from @ Volodymyr123rusofob/project-CodeCrafters@e75c8a4539bc634dbae985634a307d451ffb6ef1 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,67 +1,67 @@
-/* empty css                      */import{a as c}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const a of o)if(a.type==="childList")for(const d of a.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&r(d)}).observe(document,{childList:!0,subtree:!0});function s(o){const a={};return o.integrity&&(a.integrity=o.integrity),o.referrerpolicy&&(a.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?a.credentials="include":o.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function r(o){if(o.ep)return;o.ep=!0;const a=s(o);fetch(o.href,a)}})();class u{constructor(){this.BASE_URL="https://food-boutique.b.goit.study/api"}async getProductsByName(t,s,r=1,o=6){const a=new URLSearchParams({keyword:t,category:s,page:r,limit:o});try{return(await c.get(`${this.BASE_URL}/products?${a}`)).data}catch{throw console.error("Помилка при отриманні продуктів:",error.message),error}}async getAllProducts(t=1,s=6){try{return(await c.get(`${this.BASE_URL}/products`,{params:{page:t,limit:s}})).data}catch(r){throw console.error("Помилка при отриманні всіх продуктів:",r.message),r}}async getPopularProducts(t){try{const s={limit:t};return(await c.get(`${this.BASE_URL}/products/popular`,{params:s})).data}catch(s){throw console.error("Помилка при отриманні популярних продуктів:",s.message),s}}async getDiscountedProducts(){try{return(await c.get(`${this.BASE_URL}/products/discount`)).data}catch(t){throw console.error("Помилка при отриманні продуктів зі знижкою:",t.message),t}}async getProductsByCategory(){try{return(await c.get(`${this.BASE_URL}/products/categories`)).data}catch(t){throw console.error("Помилка при отриманні продуктів за категорією:",t.message),t}}async getProductsInCategories(t,s=1,r=6){try{const o=new URLSearchParams({category:t,page:s,limit:r});return(await c.get(`${this.BASE_URL}/products?${o}`)).data}catch(o){throw console.error("Помилка при отриманні продуктів за категорією(при оновленні сторінки): ",o.message),o}}async getProductById(t){try{return(await c.get(`${this.BASE_URL}/products/${t}`)).data}catch(s){throw console.error("Помилка при отриманні продукта за айді:",s.message),s}}async createNewOrder(t){try{return(await c.post(`${this.BASE_URL}/orders`,t)).data}catch(s){throw console.error("Помилка при створенні нового замовлення:",s.message),s}}async createSubscription(t){try{return(await c.post(`${this.BASE_URL}/subscriptions`,{email:t})).data}catch(s){throw console.error("Помилка при створенні підписки:",s.message),s}}}const i="/project-CodeCrafters/assets/symbol-defs-70073224.svg",n=document.querySelector(".modal-prod-wrapper"),y=new u;async function v(e){n.classList.add("modal-active"),n.classList.add("loader");const t=await y.getProductById(e);n.classList.remove("loader"),L(t),w(t)}function w(e){const t=document.querySelector(".modal-prod-add-text"),s=e._id;shopStorage.getAllProducts().some(o=>o._id===s)?t.textContent="Remove from":t.textContent="Add to"}function L(e){try{n.classList.add("modal-active"),document.body.classList.add("stop-scroll"),n.innerHTML=`
+var $=(s,t,e)=>{if(!t.has(s))throw TypeError("Cannot "+e)};var h=(s,t,e)=>{if(t.has(s))throw TypeError("Cannot add the same private member more than once");t instanceof WeakSet?t.add(s):t.set(s,e)};var l=(s,t,e)=>($(s,t,"access private method"),e);/* empty css                      */import{a as c}from"./assets/vendor-a61d8330.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const a of o)if(a.type==="childList")for(const i of a.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function e(o){const a={};return o.integrity&&(a.integrity=o.integrity),o.referrerpolicy&&(a.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?a.credentials="include":o.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function r(o){if(o.ep)return;o.ep=!0;const a=e(o);fetch(o.href,a)}})();var n,u;class k{constructor(t){h(this,n);this.storageBasketName=t,this.initStorage()}initStorage(){this.getAllProducts()||l(this,n,u).call(this,[])}getAllProducts(){return JSON.parse(localStorage.getItem(this.storageBasketName))}addProduct(t){if(!t)return;const e=this.getAllProducts(),r=e.find(o=>o._id===t._id);r?r.amount+=1:(t.amount=1,e.push(t)),l(this,n,u).call(this,e)}removeProductById(t){const e=this.getAllProducts(),r=e.find(o=>o._id===t);if(r)r.amount>1?r.amount-=1:e.filter(o=>o._id===r._id);else return;l(this,n,u).call(this,e)}removeAllProductsById(t){l(this,n,u).call(this,this.getAllProducts().filter(e=>e._id===t))}removeAllProducts(){l(this,n,u).call(this,[])}}n=new WeakSet,u=function(t){localStorage.setItem(this.storageBasketName,JSON.stringify(t))};class f{constructor(){this.BASE_URL="https://food-boutique.b.goit.study/api"}async getProductsByName(t,e,r=1,o=6){const a=new URLSearchParams({keyword:t,category:e,page:r,limit:o});try{return(await c.get(`${this.BASE_URL}/products?${a}`)).data}catch{throw console.error("Помилка при отриманні продуктів:",error.message),error}}async getAllProducts(t=1,e=6){try{return(await c.get(`${this.BASE_URL}/products`,{params:{page:t,limit:e}})).data}catch(r){throw console.error("Помилка при отриманні всіх продуктів:",r.message),r}}async getPopularProducts(t){try{const e={limit:t};return(await c.get(`${this.BASE_URL}/products/popular`,{params:e})).data}catch(e){throw console.error("Помилка при отриманні популярних продуктів:",e.message),e}}async getDiscountedProducts(){try{return(await c.get(`${this.BASE_URL}/products/discount`)).data}catch(t){throw console.error("Помилка при отриманні продуктів зі знижкою:",t.message),t}}async getProductsByCategory(){try{return(await c.get(`${this.BASE_URL}/products/categories`)).data}catch(t){throw console.error("Помилка при отриманні продуктів за категорією:",t.message),t}}async getProductsInCategories(t,e=1,r=6){try{const o=new URLSearchParams({category:t,page:e,limit:r});return(await c.get(`${this.BASE_URL}/products?${o}`)).data}catch(o){throw console.error("Помилка при отриманні продуктів за категорією(при оновленні сторінки): ",o.message),o}}async getProductById(t){try{return(await c.get(`${this.BASE_URL}/products/${t}`)).data}catch(e){throw console.error("Помилка при отриманні продукта за айді:",e.message),e}}async createNewOrder(t){try{return(await c.post(`${this.BASE_URL}/orders`,t)).data}catch(e){throw console.error("Помилка при створенні нового замовлення:",e.message),e}}async createSubscription(t){try{return(await c.post(`${this.BASE_URL}/subscriptions`,{email:t})).data}catch(e){throw console.error("Помилка при створенні підписки:",e.message),e}}}const p="/project-CodeCrafters/assets/symbol-defs-9748d683.svg",d=document.querySelector(".modal-prod-wrapper"),B="cart",y=new k(B),b=new f;async function A(s){d.classList.add("modal-active"),d.classList.add("loader");const t=await b.getProductById(s);d.classList.remove("loader"),I(t),E(t)}function E(s){const t=document.querySelector(".modal-prod-add-text"),e=s._id;y.getAllProducts().some(o=>o.productId===e)?t.textContent="Remove from":(console.log("sdvvdsfgd"),t.textContent="Add to")}function I(s){try{d.classList.add("modal-active"),document.body.classList.add("stop-scroll"),d.innerHTML=`
   <div class="modal-prod-card">
   <button type="button" class="modal-prod-close-btn">
     <svg class="modal-prod-close-icon" width="22" height="22">
-      <use href="${i}#icon-ion_close-sharp"></use>
+      <use href="${p}#icon-ion_close-sharp"></use>
     </svg>
   </button>
   <div class="modal-prod-information-wrap">
   <div class="modal-prod-img-wrap">
-      <img class="modal-prod-img" src="${e.img}" alt="${e.name}" />
+      <img class="modal-prod-img" src="${s.img}" alt="${s.name}" />
   </div>
   <div class="modal-prod-name-wrap">
-  <p class="modal-prod-name">${e.name}</p>
+  <p class="modal-prod-name">${s.name}</p>
   <ul class="modal-prod-list">
     <li class="modal-prod-item">
-      <p class="modal-prod-text">Category: <span>${e.category}</span></p>
+      <p class="modal-prod-text">Category: <span>${s.category}</span></p>
     </li>
     <li class="modal-prod-item">
-      <p class="modal-prod-text">Size: <span>${e.size}</span></p>
+      <p class="modal-prod-text">Size: <span>${s.size}</span></p>
     </li>
     <li class="modal-prod-item">
-      <p class="modal-prod-text">Popularity: <span>${e.popularity}</span></p>
+      <p class="modal-prod-text">Popularity: <span>${s.popularity}</span></p>
     </li>
   </ul>
-  <p class="modal-prod-desc">${e.desc}</p>
+  <p class="modal-prod-desc">${s.desc}</p>
   </div>
   </div>
   <div class="modal-prod-price-elem">
-  <p class="modal-prod-price">&#36;${e.price}</p>
+  <p class="modal-prod-price">&#36;${s.price}</p>
   <button class="modal-prod-add-btn" >
       <p class="modal-prod-add-text">Add to</p>
       <svg class="modal-prod-basket-icon" >
-        <use href="${i}#icon-basket"></use>
+        <use href="${p}#icon-basket"></use>
       </svg>
   </button>
   </div>
   </div>
-  `,document.querySelector(".modal-prod-add-btn").addEventListener("click",()=>$(e)),document.querySelector(".modal-prod-close-btn").addEventListener("click",()=>p()),window.addEventListener("click",m),window.addEventListener("keydown",g)}catch(t){console.error(t)}}function $(e){const t=e._id,r=shopStorage.getAllProducts().some(a=>a._id===t),o=document.querySelector(".modal-prod-add-text");r?o.textContent="Add to":o.textContent="Remove from"}function p(){document.body.classList.remove("stop-scroll"),n.classList.remove("modal-active"),n.innerHTML="",window.removeEventListener("click",m),window.removeEventListener("keydown",g)}function m(e){e.target===n&&p()}function g(e){e.keyCode===27&&p()}function S(e){return e.map(({_id:t,name:s,img:r,category:o,price:a,size:d,popularity:f})=>`
+  `,document.querySelector(".modal-prod-add-btn").addEventListener("click",()=>C(s)),document.querySelector(".modal-prod-close-btn").addEventListener("click",()=>g()),window.addEventListener("click",v),window.addEventListener("keydown",w)}catch(t){console.error(t)}}function C(s){const t=s._id,r=y.getAllProducts().some(a=>a.productId===t),o=document.querySelector(".modal-prod-add-text");r?o.textContent="Add to":(o.textContent="Remove from",L(o,t))}function g(){document.body.classList.remove("stop-scroll"),d.classList.remove("modal-active"),d.innerHTML="",window.removeEventListener("click",v),window.removeEventListener("keydown",w)}function v(s){s.target===d&&g()}function w(s){s.keyCode===27&&g()}function _(s){return s.map(({_id:t,name:e,img:r,category:o,price:a,size:i,popularity:P})=>`
             <li class="product-item" data-product-id='${t}'>
               <div class="product-img-container">
                 <a class="products-card-link" href="">
-                  <img src="${r}" alt="${s}" width="140" height="140" class="product-img">
+                  <img src="${r}" alt="${e}" width="140" height="140" class="product-img">
                 </a>
               </div>
                 <a class="products-card-link" href="">
-                  <h3 class="product-title">${s}</h3>
+                  <h3 class="product-title">${e}</h3>
                 </a>
               <div class="product-parameter-container">
                 <p class="product-parameter">Category: <span class="span-parameter-value"> ${o.replace(/_/g," ")}</span></p>
-                <p class="product-parameter">Size: <span class="span-parameter-value"> ${d}</span></p>
-                <p class="product-parameter popularity">Popularity: <span class="span-parameter-value"> ${f}</span></p>
+                <p class="product-parameter">Size: <span class="span-parameter-value"> ${i}</span></p>
+                <p class="product-parameter popularity">Popularity: <span class="span-parameter-value"> ${P}</span></p>
                 </div>
               <div class="price-card-container">
                 <p class="price">$${a}</p>
                 <button class="cart-button" type="button" data-item-id="${t}" data-in-cart="false">
                   <svg class="cart-icon" width="18" height="18">
-                    <use href="${i}#icon-basket"></use>
+                    <use href="${p}#icon-basket"></use>
                   </svg>
                 </button>
               </div>
             </li>
-            `).join("")}const h=document.querySelector(".js-products-list");h.addEventListener("click",_);const b=new u,k=6;async function B(){const t=(await b.getAllProducts()).results.slice(0,k);P(t,h),C()}function P(e,t){t.innerHTML=S(e)}const l=JSON.parse(localStorage.getItem("cart"))||[];function C(){E()}function E(){const e=document.querySelectorAll(".cart-button");e&&e.forEach(t=>{t.addEventListener("click",A)})}function A(e){const t=e.target.closest(".cart-button");if(t){const s=t.dataset.itemId;I(t,s)}}function I(e,t){l.findIndex(r=>r.productId===t)!==-1?alert("Цей продукт вже доданий до кошика!"):l.push({productId:t,quantity:1}),x(e,!0),localStorage.setItem("cart",JSON.stringify(l))}function x(e,t){t?e.innerHTML=`<svg class="cart-icon" width="18" height="18">
-        <use href="${i}#icon-check"></use>
-        </svg>`:e.innerHTML=`<svg class="cart-icon" width="18" height="18">
-        <use href="${i}#icon-basket"></use>
-        </svg>`}B();function _(e){e.preventDefault();const t=e.target;if(t.closest("a")&&t.closest(".products-card-link")){const s=t.closest("li").dataset.productId;v(s).catch(r=>{console.error("Помилка при отриманні продукта за айді:",r.message)})}}
+            `).join("")}const S=document.querySelector(".js-products-list");S.addEventListener("click",H);const x=new f,R=6;async function O(){const t=(await x.getAllProducts()).results.slice(0,R);M(t,S),N()}function M(s,t){t.innerHTML=_(s)}const m=JSON.parse(localStorage.getItem("cart"))||[];function N(){T()}function T(){const s=document.querySelectorAll(".cart-button");s&&s.forEach(t=>{t.addEventListener("click",U)})}function U(s){const t=s.target.closest(".cart-button");if(t){const e=t.dataset.itemId;L(t,e)}}function L(s,t){m.findIndex(r=>r.productId===t)!==-1?alert("Цей продукт вже доданий до кошика!"):m.push({productId:t,quantity:1}),q(s,!0),localStorage.setItem("cart",JSON.stringify(m))}function q(s,t){t?s.innerHTML=`<svg class="cart-icon" width="18" height="18">
+        <use href="${p}#icon-check"></use>
+        </svg>`:s.innerHTML=`<svg class="cart-icon" width="18" height="18">
+        <use href="${p}#icon-basket"></use>
+        </svg>`}O();function H(s){s.preventDefault();const t=s.target;if(console.log(t),t.closest("a")&&t.closest(".products-card-link")){const e=t.closest("li").dataset.productId;A(e).catch(r=>{console.error("Помилка при отриманні продукта за айді:",r.message)})}}
 //# sourceMappingURL=commonHelpers2.js.map
