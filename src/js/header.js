3d9cb82754f9770e0d2c8911435969dac50bf30a
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2023-12-10 23:10:57 +0200

    1

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,27 @@
-const LS_KEY = 'productsBasket';
+// const LS_KEY = 'productsBasket';
 
-const loadFromLS = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
+// const loadFromLS = key => {
+//   try {
+//     const serializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
 
+// export const updateCartCounter = () => {
+//   const cartCounter = document.querySelector('#cart-counter');
+
+//   const items = loadFromLS(LS_KEY) ?? [];
+//   cartCounter.textContent = `(${items.length})`;
+// };
+
+// updateCartCounter();
+import { getAllProducts } from './local-storage-interface.js';
 export const updateCartCounter = () => {
   const cartCounter = document.querySelector('#cart-counter');
-
-  const items = loadFromLS(LS_KEY) ?? [];
+  const items = getAllProducts();
   cartCounter.textContent = `(${items.length})`;
 };
-
 updateCartCounter();
+
