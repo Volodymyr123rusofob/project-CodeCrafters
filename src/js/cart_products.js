126c72b885871508e30751d8d5ec0ca00734024f
Tanya Yenakieva <yastigma@gmail.com>
2023-12-10 09:33:08 +0200

    intermediate
    У робочому стані відмалювання розмітки з отриманого ЛС інформацією та очищення

--- src/js/cart_products.js
+++ src/js/cart_products.js
@@ -1,5 +1,9 @@
+
+
 import ApiService from './requests';
 import icons from '../img/symbol-defs.svg'; 
+
+
 import { ShopStorage } from './local-storage';
 import { getEmptyCartMarkup, getCartMarkup, getcheckoutMarkup } from './markup_cart';
 
@@ -7,32 +11,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const basket = document.getElementById('basket');
     const cartTitle = document.getElementById('cart-counter');
-    const btnDeleteAll = document.querySelector('.btn-delete-all');
 
 
 
     const storage = new ShopStorage('cart');
-    const products = storage.getAllProducts();
     // Формування розмітки контейнерів. якщо кошик порожній відмальовується Empty, у іншову випадку відмальовується розмітка CART
+ function updateCart() {
+    const products = storage.getAllProducts();
+
     if (products.length === 0) {
-        basket.innerHTML = '';
-        basket.insertAdjacentHTML('beforeend', getEmptyCartMarkup());
+      basket.innerHTML = '';
+      basket.insertAdjacentHTML('beforeend', getEmptyCartMarkup());
     } else {
-        basket.innerHTML = '';
-        basket.insertAdjacentHTML('beforeend', getCartMarkup(products));
-        basket.insertAdjacentHTML('beforeend', getcheckoutMarkup());
+      basket.innerHTML = '';
+      basket.insertAdjacentHTML('beforeend', getCartMarkup(products));
+      basket.insertAdjacentHTML('beforeend', getcheckoutMarkup());
     }
 
-    updateCartCounter();
+    const productCount = products.length;
+    cartTitle.textContent = `CART (${productCount})`;
+  }
 
-    function updateCartCounter() {
-        const productCount = products.length;
-        cartTitle.textContent = `CART (${productCount})`;
-    }
-    btnDeleteAll.addEventListener('click', () => {
+  updateCart();
 
-        storage.removeAllProducts();  // Очищайте локальное хранилище
-        updateCartMarkup();
-   
-    });
+  const btnDeleteAll = document.querySelector('.btn-delete-all');
+  btnDeleteAll.addEventListener('click', () => {
+    storage.removeAllProducts();
+    updateCart();
+  });
 });
--- src/js/markup_cart.js
+++ src/js/markup_cart.js
@@ -45,42 +45,41 @@ export const getEmptyCartMarkup = () =>
 
 export const getCartMarkup = (products) =>
     `
-    <div class="delete-all">
-      <button type="button" class="btn-delete-all"> Delete All 
-        <span class="icon-close-sharp">
-          <svg class="icon-basket" width="24" height="24">
-            <use href="/img/symbol-defs.svg#icon-ion_close-sharp"></use>
-          </svg>
-        </span>
-      </button>
-    </div>
-    <ul class="cart-products-list">
-     ${products.map(({ _id, name, img, category, size, price }) => `
-  <li class="cart-product-item" data-product-id="${_id}">
-    
-    <div class="cart-product-container">
-      <div class="cart-product-img-container">
-        <img src="${img}" alt="${name}" width="64" height="64" class="cart-product-img">
-      </div>
-      <div class="cart-product-parameter-container">
-        <h2 class="cart-product-title">${name}</h2>
-        <p class="cart-product-parameter">Category: <span class="cart-span-parameter-value">${category ? category.replace(/_/g, ' ') : ''}</span></p>
-        <p class="cart-product-parameter">Size: <span class="cart-span-parameter-value">${size}</span></p>
-        <p class="price">$${price}</p>
-      </div>
-      <div class="cart-delete">
-      <button type="button" class="cart-btn-delete">
-        <span class="cart-icon-close-sharp">
-          <svg class="icon-basket" width="24" height="24">
-            <use href="/img/symbol-defs.svg#icon-ion_close-sharp"></use>
-          </svg>
-        </span>
-      </button>
-    </div>
-    </div>
-  </li>`).join('')}
-    </ul>
-  `;
+     <div class="delete-all">
+    <button type="button" class="btn-delete-all"> Delete All 
+      <span class="icon-close-sharp">
+        <svg class="icon-basket" width="24" height="24">
+          <use href="/img/symbol-defs.svg#icon-ion_close-sharp"></use>
+        </svg>
+      </span>
+    </button>
+  </div>
+  <ul class="cart-products-list">
+    ${products.map(({ _id, name, img, category, size, price }) => `
+      <li class="cart-product-item" data-product-id="${_id}">
+        <div class="cart-product-container">
+          <div class="cart-product-img-container">
+            <img src="${img}" alt="${name}" width="64" height="64" class="cart-product-img">
+          </div>
+          <div class="cart-product-parameter-container">
+            <h2 class="cart-product-title">${name}</h2>
+            <p class="cart-product-parameter">Category: <span class="cart-span-parameter-value">${category ? category.replace(/_/g, ' ') : ''}</span></p>
+            <p class="cart-product-parameter">Size: <span class="cart-span-parameter-value">${size}</span></p>
+            <p class="price">$${price}</p>
+          </div>
+          <div class="cart-delete">
+            <button type="button" class="cart-btn-delete">
+              <span class="cart-icon-close-sharp">
+                <svg class="icon-basket" width="24" height="24">
+                  <use href="/img/symbol-defs.svg#icon-ion_close-sharp"></use>
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
+      </li>`).join('')}
+  </ul>
+`;
 
 export const getcheckoutMarkup = () =>`
      <h2 class="checkout-title">Your order</h2>
