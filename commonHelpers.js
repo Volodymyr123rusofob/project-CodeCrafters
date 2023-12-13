b86a5e25dd21cdfeef9f338d88efb2dfcf788fb8
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-12-13 15:55:56 +0000

    Deploying to gh-pages from @ Volodymyr123rusofob/project-CodeCrafters@84ad190b973baf39bef938c7e55e03dc847bce4e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as g,S as h,u as b,A as y}from"./assets/header-e9fc3948.js";import"./assets/vendor-ab16d78c.js";const C="/project-CodeCrafters/assets/empty-cart-desktop-1x-1da79769.png",$="/project-CodeCrafters/assets/empty-cart-desktop-2x-490faf55.png",D="/project-CodeCrafters/assets/empty-cart-desktop-1x-1da79769.png",w="/project-CodeCrafters/assets/empty-cart-desktop-2x-490faf55.png",E="/project-CodeCrafters/assets/empty-cart-mob-1x-971d1e28.png",P="/project-CodeCrafters/assets/empty-cart-mob-2x-7db9012b.png",f=()=>`<div class="empty-cart">
+import{i as g,S as h,u as b,A as y}from"./assets/header-bb30abe4.js";import"./assets/vendor-ab16d78c.js";const C="/project-CodeCrafters/assets/empty-cart-desktop-1x-1da79769.png",$="/project-CodeCrafters/assets/empty-cart-desktop-2x-490faf55.png",D="/project-CodeCrafters/assets/empty-cart-desktop-1x-1da79769.png",w="/project-CodeCrafters/assets/empty-cart-desktop-2x-490faf55.png",E="/project-CodeCrafters/assets/empty-cart-mob-1x-971d1e28.png",P="/project-CodeCrafters/assets/empty-cart-mob-2x-7db9012b.png",f=()=>`<div class="empty-cart">
       <div class="empty-picture">
       <picture>
           <source
