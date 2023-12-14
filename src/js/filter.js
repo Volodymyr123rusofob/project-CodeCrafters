f20718315b81aece73fd1b318cb1fd3cbce599e2
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-14 09:00:15 +0200

    Update filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,120 +1,115 @@
-
 import createMarkup from './markup_products_list.js';
 import ApiService from './requests.js';
-import { addEventListenersToBasketButtons, displayProducts } from './products_list.js';
-import {FilterStorage} from './filter_helpers.js'
+import {
+  addEventListenersToBasketButtons,
+  displayProducts,
+} from './products_list.js';
+// import { FilterStorage } from './filter_helpers.js';
 // import {alertPopUp} from './alert';
 // alertPopUp('The product has been removed from the basket!');
 import Pagination from 'tui-pagination';
 
 document.addEventListener('DOMContentLoaded', async () => {
-
   localStorage.setItem('noResultsMessageDisplayed', 'false');
   const apiService = new ApiService();
 
   // Запит списку категорій та додавання "Show all"
   const categorySelect = document.querySelector('.category-select');
 
-   categorySelect.addEventListener('change', async () => {
+  categorySelect.addEventListener('change', async () => {
     await filterProducts();
-   });
-  
-   async function filterProducts() {
-     // Получение значений из формы и локального хранилища
-     const keywordInput = document.querySelector('.keyword-input');
-     const selectedCategory = categorySelect.value;
-     const filters = {
-       keyword: keywordInput.value || null,
-       category:
-         selectedCategory === 'Show all'
-           ? categorySelect.selectedCategory=""
-           : selectedCategory,
-       page: 1,
-       limit: 9,
-     };
-     localStorage.setItem('filters', JSON.stringify(filters));
+  });
 
-     // Надсилання запиту на сервер з урахуванням фільтрів
-     try {
-       let products;
+  async function filterProducts() {
+    const keywordInput = document.querySelector('.keyword-input');
+    const selectedCategory = categorySelect.value;
+    const filters = {
+      keyword: keywordInput.value || null,
+      category:
+        selectedCategory === 'Show all' || selectedCategory === 'Categories'
+          ? ''
+          : selectedCategory,
+      page: 1,
+      limit: 9,
+    };
+    localStorage.setItem('filters', JSON.stringify(filters));
+    keywordInput.value = '';
 
-       // Перевіряємо, чи є ключове слово
-       if (!filters.keyword && filters.category === null) {
-         // Якщо і ключове слово, і категорія відсутні,
-         // Запит на отримання всіх продуктів
-         products = await apiService.getAllProducts(
-           filters.page,
-           filters.limit
-         );
-       } else if (!filters.keyword) {
-         // Якщо немає ключового слова, запит на отримання всіх продуктів у вибраній категорії
-         products = await apiService.getProductsInCategories(
-           filters.category,
-           filters.page,
-           filters.limit
-         );
-       } else {
-         // Інакше запит на сервер з урахуванням ключового слова
-         products = await apiService.getProductsByName(
-           filters.keyword,
-           filters.category,
-           filters.page,
-           filters.limit
-         );
-       }
+    try {
+      let products;
 
-       // Обробка даних (наприклад, відображення на сторінці)
-       console.log('Полученные продукты:(products=>)', products);
-      
-       // Сохранение products.results в локальное хранилище для пагинации
-       //  localStorage.setItem(
-       //    'productsResults',
-       //    JSON.stringify(products.results)
-       //  );
-       //  Создаем экземпляр  FilterStorage:
-       const storageKey = 'filterOfProducts';
-       const filterStorageInstance = new FilterStorage(storageKey);
+      // Перевіряємо, чи є ключове слово
+      if (!filters.keyword && filters.category === null) {
+        // Якщо і ключове слово, і категорія відсутні,
+        // Запит на отримання всіх продуктів
+        products = await apiService.getAllProducts(filters.page, filters.limit);
+      } else if (!filters.keyword) {
+        // Якщо немає ключового слова, запит на отримання всіх продуктів у вибраній категорії
+        products = await apiService.getProductsInCategories(
+          filters.category,
+          filters.page,
+          filters.limit
+        );
+      } else {
+        // Інакше запит на сервер з урахуванням ключового слова
+        products = await apiService.getProductsByName(
+          filters.keyword,
+          filters.category,
+          filters.page,
+          filters.limit
+        );
+      }
 
-       const filterOfProducts = products.results;
-//  сохраняем массив в локальное хранилище
-       filterStorageInstance.writeToLocalStorage(filterOfProducts);
+      //  console.log('Полученные продукты:(products=>)', products);
 
-// pagination.reset();   
-       // RENDERing
-       if (products.totalPages === 0) {
-         if (
-           localStorage.getItem('noResultsMessageDisplayed') !== 'true' ||
-           localStorage.getItem('noResultsMessageDisplayed') !== true
-         ) {
-           renderNoResultsMessage();
-         }
-       } else {
-         // Передача властивостей продукту markUp()
-         const productsMarkup = createMarkup(products.results);
+      // Сохранение products.results в локальное хранилище для пагинации (totalPages)
+      localStorage.setItem('products', JSON.stringify(products));
 
-         // Відображення HTML-розмітки на сторінці
-         const productsListContainer = document.querySelector('.js-products-list');
+      //  Создаем экземпляр  FilterStorage:
+      //  const storageKey = 'filterOfProducts';
+      //  const filterStorageInstance = new FilterStorage(storageKey);
+      //  const filterOfProducts = products.results;
 
-         displayProducts(products.results, productsListContainer);
+      //  filterStorageInstance.writeToLocalStorage(filterOfProducts);
 
-         //  productsList.innerHTML = productsMarkup;
-         
-         addEventListenersToBasketButtons();
-         // Удаление сообщения "Nothing..."
-         removeNoResultsMessage();
-         // Устанавливаем флаг в false, так как результаты найдены
-         localStorage.setItem('noResultsMessageDisplayed', 'false');
-       }
-     } catch (error) {
-       console.error('Ошибка при получении продуктов:', error.message);
-     }
-   }
-  
+      // pagination.reset();
+      // RENDERing
+      if (products.totalPages === 0) {
+        if (
+          localStorage.getItem('noResultsMessageDisplayed') !== 'true' ||
+          localStorage.getItem('noResultsMessageDisplayed') !== true
+        ) {
+          renderNoResultsMessage();
+        }
+      } else {
+        // Передача властивостей продукту markUp()
+        const productsMarkup = createMarkup(products.results);
+
+        // Відображення HTML-розмітки на сторінці
+        const productsListContainer =
+          document.querySelector('.js-products-list');
+
+        displayProducts(products.results, productsListContainer);
+
+        //  productsList.innerHTML = productsMarkup;
+
+        addEventListenersToBasketButtons();
+        // Удаление сообщения "Nothing..."
+        removeNoResultsMessage();
+        // Устанавливаем флаг в false, так как результаты найдены
+        localStorage.setItem('noResultsMessageDisplayed', 'false');
+      }
+    } catch (error) {
+      console.error('Ошибка при получении продуктов:', error.message);
+    }
+  }
+
   try {
     const categories = await apiService.getProductsByCategory();
 
-    categories.push('Show all');
     categorySelect.innerHTML = '';
+    categories.unshift('Categories');
+    categories.push('Show all');
 
     categories.forEach(category => {
       const option = document.createElement('option');
@@ -149,17 +144,16 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     localStorage.setItem('filters', JSON.stringify(filters));
 
-   
     const defaultFilters = {
-    keyword: null,
-    category: null,
-    page: 1,
-    limit: 9,
-  };
-  localStorage.setItem('filters', JSON.stringify(defaultFilters));
+      keyword: null,
+      category: null,
+      page: 1,
+      limit: 9,
+    };
+    localStorage.setItem('filters', JSON.stringify(defaultFilters));
 
-  // Вызов функции фильтрации при загрузке страницы
-  await filterProducts();
+    // Вызов функции фильтрации при загрузке страницы
+    await filterProducts();
   });
 
   function removeNoResultsMessage() {
@@ -209,11 +203,10 @@ function addFocusStyle() {
 function removeFocusStyle() {
   inputBox.classList.remove('focus-within');
 }
-// 
-// 
+//
+//
 // rendering "Nothing was found for the selected"
 function renderNoResultsMessage() {
-
   if (
     localStorage.getItem('noResultsMessageDisplayed') === 'true' ||
     localStorage.getItem('noResultsMessageDisplayed') === true
@@ -249,5 +242,3 @@ function renderNoResultsMessage() {
   // Устанавливаем флаг в локальное хранилище, чтобы помнить, что сообщение уже было выведено
   localStorage.setItem('noResultsMessageDisplayed', 'true');
 }
-
-
--- src/js/filter_helpers.js
+++ src/js/filter_helpers.js
@@ -1,27 +1,27 @@
-export class FilterStorage {
-  constructor(storageFilter) {
-    this.storageFilter = storageFilter;
-    // this.initStorageForFilter();
-  }
+// export class FilterStorage {
+//   constructor(storageFilter) {
+//     this.storageFilter = storageFilter;
+//     // this.initStorageForFilter();
+//   }
 
-  initStorageForFilter() {
-    if (this.getFilterProducts()) {
-      console.log('this.storageFilter=', this.storageFilter);
-      return;
-    }
-    this.writeToLocalStorage([]);
-    console.log('this.storageFilter-[]');
-  }
+//   initStorageForFilter() {
+//     if (this.getFilterProducts()) {
+//       console.log('this.storageFilter=', this.storageFilter);
+//       return;
+//     }
+//     this.writeToLocalStorage([]);
+//     console.log('this.storageFilter-[]');
+//   }
 
-  getFilterProducts() {
-    return JSON.parse(localStorage.getItem(this.storageFilter));
-  }
+//   getFilterProducts() {
+//     return JSON.parse(localStorage.getItem(this.storageFilter));
+//   }
 
-  removeFilterProducts() {
-    this.writeToLocalStorage([]);
-  }
+//   removeFilterProducts() {
+//     this.writeToLocalStorage([]);
+//   }
 
-  writeToLocalStorage(filterArray) {
-    localStorage.setItem(this.storageFilter, JSON.stringify(filterArray));
-  }
-}
+//   writeToLocalStorage(filterArray) {
+//     localStorage.setItem(this.storageFilter, JSON.stringify(filterArray));
+//   }
+// }
